5f5fe3e7c92bfee77b4e9930242ba6a5227bb45d
IacobIlinca <126677598+IacobIlinca@users.noreply.github.com>
2023-03-07 08:45:28 +0200

    lap7-oop

---
+++ Year1/Second semester/OOP/lab 7/VectorDinamicCPP (1)/VectorDinamicCPP/VectorDinamicCPP/cmake-build-debug/CMakeFiles/VectorDinamicCPP.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/Ilinca/Desktop/facultate/semestru 2/oop/lab 7/VectorDinamicCPP (1)/VectorDinamicCPP/VectorDinamicCPP/Pet.cpp" "/mnt/c/Users/Ilinca/Desktop/facultate/semestru 2/oop/lab 7/VectorDinamicCPP (1)/VectorDinamicCPP/VectorDinamicCPP/cmake-build-debug/CMakeFiles/VectorDinamicCPP.dir/Pet.cpp.o"
+  "/mnt/c/Users/Ilinca/Desktop/facultate/semestru 2/oop/lab 7/VectorDinamicCPP (1)/VectorDinamicCPP/VectorDinamicCPP/VectDinNewDelete.cpp" "/mnt/c/Users/Ilinca/Desktop/facultate/semestru 2/oop/lab 7/VectorDinamicCPP (1)/VectorDinamicCPP/VectorDinamicCPP/cmake-build-debug/CMakeFiles/VectorDinamicCPP.dir/VectDinNewDelete.cpp.o"
+  "/mnt/c/Users/Ilinca/Desktop/facultate/semestru 2/oop/lab 7/VectorDinamicCPP (1)/VectorDinamicCPP/VectorDinamicCPP/VectDinSmartPointer.cpp" "/mnt/c/Users/Ilinca/Desktop/facultate/semestru 2/oop/lab 7/VectorDinamicCPP (1)/VectorDinamicCPP/VectorDinamicCPP/cmake-build-debug/CMakeFiles/VectorDinamicCPP.dir/VectDinSmartPointer.cpp.o"
+  "/mnt/c/Users/Ilinca/Desktop/facultate/semestru 2/oop/lab 7/VectorDinamicCPP (1)/VectorDinamicCPP/VectorDinamicCPP/main.cpp" "/mnt/c/Users/Ilinca/Desktop/facultate/semestru 2/oop/lab 7/VectorDinamicCPP (1)/VectorDinamicCPP/VectorDinamicCPP/cmake-build-debug/CMakeFiles/VectorDinamicCPP.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Year1/Second semester/OOP/lab 7/VectorDinamicCPP (1)/VectorDinamicCPP/VectorDinamicCPP/cmake-build-debug/CMakeFiles/VectorDinamicCPP.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VectorDinamicCPP.dir/Pet.cpp.o"
+  "CMakeFiles/VectorDinamicCPP.dir/VectDinNewDelete.cpp.o"
+  "CMakeFiles/VectorDinamicCPP.dir/VectDinSmartPointer.cpp.o"
+  "CMakeFiles/VectorDinamicCPP.dir/main.cpp.o"
+  "VectorDinamicCPP"
+  "VectorDinamicCPP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VectorDinamicCPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
